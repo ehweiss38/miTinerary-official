2fd74a1cb8186981c4eda2b58d25ec874b3fe96a
ehweiss38 <ehweiss38@gmail.com>
2022-12-14 15:57:10 -0500

    c

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-//import {disableReactDevTools} from '@fvilers/disable-react-devtools'
+//import reportWebVitals from './reportWebVitals';
+import {disableReactDevTools} from '@fvilers/disable-react-devtools'
 
 if(process.env.NODE_ENV==='production') disableReactDevTools()
 
